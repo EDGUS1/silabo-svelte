21ac2422645c110b567b63ee403586e156fb820a
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-10-06 23:53:06 -0500

    feat: add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,7 @@
 <script>
   import Header from './components/Header.svelte';
-  import HomeSilabos from './components/HomeSilabos.svelte';
-
-  import Login from './components/Login.svelte';
 </script>
 
 <main>
   <Header />
-  <!-- <Login /> -->
-  <HomeSilabos />
 </main>
-
-<style>
-</style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,27 +1,41 @@
-<div class="navbar navbar-expand-md navbar-light bg-light">
-  <div class="container">
-    <a href="/" class="navbar-brand">Logo</a>
-    <button
-      class="navbar-toggler collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarResponsive"
-      aria-controls="navbarResponsive"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="navbar-collapse collapse" id="navbarResponsive">
-      <ul class="navbar-nav ms-md-auto">
-        <li class="nav-item mx-3">
-          <a class="nav-link" href="/silabos">Silabos</a>
-        </li>
-        <li class="nav-item">
-          <!-- <a class="btn btn-success" href="/" > Login </a> -->
-          <button class="btn btn-danger"> Logout </button>
-        </li>
-      </ul>
+<script>
+  import { Router, Link, Route } from 'svelte-routing';
+  import HomeSilabos from './HomeSilabos.svelte';
+
+  import Login from './Login.svelte';
+  export let url = '';
+</script>
+
+<Router {url}>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+      <Link to="/" class="navbar-brand">Logo</Link>
+      <button
+        class="navbar-toggler collapsed"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="navbar-collapse collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-md-auto">
+          <li class="nav-item mx-3">
+            <Link to="silabo" class="nav-link">Silabos</Link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="btn btn-success" href="/" > Login </a> -->
+            <button class="btn btn-danger"> Logout </button>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div>
+    <Route path="silabo" component={HomeSilabos} />
+    <Route path="/"><Login /></Route>
   </div>
-</div>
+</Router>
